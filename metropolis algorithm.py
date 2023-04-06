11a696fb4fd9c585cffb1a0e9f61f4e14f5c099d
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-04-06 23:14:17 +0100

    updated beta_plot and added binning()
    binning isnt really working but is being left for now to hopefully be fixed later

--- metropolis algorithm.py
+++ metropolis algorithm.py
@@ -33,8 +33,8 @@ def Z(T,x,N):
     total = 0
     k = 1
     beta = 1/(k*T)
-    metro_elements = metro(x, N)
-    for n in range(len(metro(x, N))):
+    metro_elements = metro(x,N,T)
+    for n in range(len(metro(x,N,T))):
         total += np.exp(-beta*(metro_elements[n]**2))
     return total
     
@@ -43,8 +43,8 @@ def P(T,x,N):
     k = 1
     P_array = np.zeros(N+1)
     beta = 1/(k*T)
-    metro_elements = metro(x, N)
-    for n in range(len(metro(x, N))):
+    metro_elements = metro(x,N,T)
+    for n in range(len(metro(x,N,T))):
         P_array[n] = np.exp(-beta*metro_elements[n]**2)/Z(T,x,N)
     return P_array
 
@@ -52,20 +52,54 @@ def U(T,x,N):
     k=1
     beta = 1/(k*T)
     integral_elements = np.zeros(N)
-    metro_elements = metro(x, N)
+    metro_elements = metro(x,N,T)
     for n in range(N):
         integral_elements[n] = (metro_elements[n]**2)*np.exp(-beta*(metro_elements[n])**2)
     U = sum(integral_elements)/Z(T,x,N)
     return U, (1/(2*beta))
     
     
-def beta_plot(T,T_N,x,N):   #T = run from 0 to temp T, T_N number of T's to sample
+def beta_plot(T_s,T,T_N,x,N):   #T = run from T_s to temp T, T_N number of T's to sample, T_s starting temperature
     k = 1
-    T_array = np.linspace(1,T,T_N)
+    T_array = np.linspace(T_s,T,T_N)
     beta_array = 1/(T_array*k)
-    metro_values = np.zeros((N+1,T_N))
-    for n in range(1,T_N):
-        metro_values[:,n] = metro(x,N,T_array[n])
-    plt.plot(beta_array, metro_values[1])
+    Energy_values = np.zeros((T_N,N+1))
+    for n in range(T_s,T_N):
+        Energy_values[n,:] = metro(x,N,T_array[n])**2
+    '''
+    b_s = 5 # bin size
+    c_list = [c for c in range(N)]
+    rolling_count = 0
+    bins_array = np.zeros((T_N, N+1))
+    for i in range(1,T_N):
+        for c in range(N):
+            for b in range(b_s):
+                rolling_count += Energy_values[i,c]
+                if b < b_s:
+                    bin_average = rolling_count/b_s
+                    bins_array[i,c] = bin_average
+                    rolling_count = 0
+    plt.plot(beta_array, bins_array[:,c_list]) # second element in bin array as c_list
+    plt.xlabel("Inverse T (beta = 1/(k*T)")
+    plt.ylabel("H(x) = x^2")
     plt.xscale("log")
-        
+    '''
+    plt.plot(beta_array, Energy_values)
+    return Energy_values
+
+
+def binning(T,T_N,x,N,b_s):
+    rolling_count = 0
+    bin_average = 0
+    data_to_bin = metro(x,N,T)
+    bin_array = np.zeros(int(N/b_s)+1)
+    for c in range(N):
+        for b in range(b_s):
+            if c+b > N:
+                break
+            rolling_count += data_to_bin[c+b]
+            if b < b_s:
+                bin_average = rolling_count/b_s
+                rolling_count = 0
+                bin_array[b] = bin_average
+    return bin_array
