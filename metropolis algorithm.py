b65b13eba0500b43d34df88e11b9eea0f548ca2a
SWalters26 <124592626+SWalters26@users.noreply.github.com>
2023-04-07 11:09:36 +0100

    reweight function added

--- metropolis algorithm.py
+++ metropolis algorithm.py
@@ -11,7 +11,7 @@ def metro(x,N,T):
     x_array[count] = x
     x_candidate = x
     while count < N:
-        x_candidate = x*rand.uniform(0,2)
+        x_candidate = x*rand.uniform(0.1,1.9)
         delta_H = (x_array[count])**2-x_candidate**2
         if np.exp(-beta*delta_H) >= 1:
             count += 1 
@@ -24,7 +24,9 @@ def metro(x,N,T):
                 x_candidate = x_array[count]
                 count += 1
                 x_array[count] = x_candidate
-    return x_array
+    average_metro = np.mean(x_array)
+    uncertainty = np.sqrt((1/(N-1)))*stat.stdev(x_array)
+    return x_array,uncertainty
     
 
 def Z(T,x,N):
@@ -33,30 +35,34 @@ def Z(T,x,N):
     total = 0
     k = 1
     beta = 1/(k*T)
+    uncertainty = metro(x, N, T)[1]
     metro_elements = metro(x,N,T)
-    for n in range(len(metro(x,N,T))):
-        total += np.exp(-beta*(metro_elements[n]**2))
-    return total
+    for n in range(len(metro_elements)):
+        total += np.exp(-beta*((metro_elements[n])**2))
+    return total, uncertainty#, 1/2*np.sqrt(np.pi/beta), 
     
 def P(T,x,N):
     #exp(-beta*H)/Z
     k = 1
     P_array = np.zeros(N+1)
     beta = 1/(k*T)
-    metro_elements = metro(x,N,T)
+    metro_elements = metro(x,N,T)[0]
+    uncertainty = Z(T,x,N)[1]
+    z = Z(T,x,N)[0]
     for n in range(len(metro(x,N,T))):
-        P_array[n] = np.exp(-beta*metro_elements[n]**2)/Z(T,x,N)
-    return P_array
+        P_array[n] = np.exp(-beta*(metro_elements[n])**2)/z
+    return P_array, uncertainty
 
 def U(T,x,N):
     k=1
     beta = 1/(k*T)
     integral_elements = np.zeros(N)
-    metro_elements = metro(x,N,T)
+    metro_elements = metro(x,N,T)[0]
+    uncertainty = Z(T,x,N)[1]
     for n in range(N):
         integral_elements[n] = (metro_elements[n]**2)*np.exp(-beta*(metro_elements[n])**2)
-    U = sum(integral_elements)/Z(T,x,N)
-    return U, (1/(2*beta))
+    U = sum(integral_elements)/Z(T,x,N)[0]
+    return U, uncertainty, (1/(2*beta))
     
     
 def beta_plot(T_s,T,T_N,x,N):   #T = run from T_s to temp T, T_N number of T's to sample, T_s starting temperature
@@ -103,3 +109,21 @@ def binning(T,T_N,x,N,b_s):
                 rolling_count = 0
                 bin_array[b] = bin_average
     return bin_array
+
+
+def reweight(T, T_prime, x, N):
+    U_old = U(T,x,N)[0]
+    metro_elements = metro(x,N,T)
+    k=1
+    beta = 1/(k*T)
+    beta_prime = 1/(k*T_prime)
+    total = 0
+    total2 = 0
+    for i in range(len(metro(x,N,T))):
+        total += U_old * np.exp(-(beta_prime-beta)*(metro_elements[i]**2)) * np.exp(-(beta*(metro_elements[i]**2)))
+    for j in range(len(metro(x,N,T))):
+        total2 += np.exp(-(beta_prime-beta)*(metro_elements[i]**2)) * np.exp(-(beta*(metro_elements[i]**2)))
+    U_new = total/total2
+    U_new_Expected = 1/(2*beta_prime)
+    U_old_Expected = 1/(2*beta)
+    return U_new, U_old, U_new_Expected, U_old_Expected
